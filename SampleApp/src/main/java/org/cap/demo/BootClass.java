5bc200304f0339841b47ae852f018f42b0cad306
placid-p <purvachandge7@gmail.com>
2021-09-20 10:50:20 +0530

    Show method added

--- SampleApp/src/main/java/org/cap/demo/BootClass.java
+++ SampleApp/src/main/java/org/cap/demo/BootClass.java
@@ -5,6 +5,11 @@ public class BootClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hi");
+show();
+	}
+	public static void show()
+	{
+		System.out.println("Show method here");
 	}
 
 }
